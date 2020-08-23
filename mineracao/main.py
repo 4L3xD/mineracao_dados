f707753d87d2e4e62a33251fd448e912f51e40b1
4L3_xD <alessandra.olea@gmail.com>
2020-08-22 22:11:34 -0300

    add produto por cliente

--- mineracao/main.py
+++ mineracao/main.py
@@ -2,13 +2,15 @@ import pandas as pd
 import xlrd
 
 df = pd.read_excel('pao-pra-ja.xlsx') 
-
 print(df)
 
-for i in df.index:
-    if df['Café'][i] == 'sim':
-      print('Clientes que compraram café: {} '.format(df['N'][i]))
+produtos = ['Leite', 'Café',  'Pão', 'Mortadela', 'Suco', 'Manteiga']
 
+print('\n')
+for i in df.index:
+  for produto in produtos:
+    if df[produto][i] == 'sim':
+      print('Clientes que compraram {}: {} '.format(produto, df['N'][i]))
 
 # Análise por linha (quais produtos cada cliente consumiu)
 # Análise por coluna (quantos clientes consumiram determinado produto)
