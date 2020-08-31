8bcd80143270ef597f52e4af2f0b315501197d29
4L3_xD <alessandra.olea@gmail.com>
2020-08-31 18:48:47 -0300

    add rigid filter

--- mineracao/atividades/lentes/main.py
+++ mineracao/atividades/lentes/main.py
@@ -73,6 +73,7 @@ nenhuma_diagHipermetrope = []
 nenhuma_jovem = []
 nenhuma_prePresbiopico = []
 nenhuma_presbiópico = []
+
 for nenhumaLente in range(len(id_nenhuma)):
   for astig in range(len(id_astigmatismo)):
     if id_nenhuma[nenhumaLente] == id_astigmatismo[astig]:
@@ -93,13 +94,33 @@ for nenhumaLente in range(len(id_nenhuma)):
     if id_nenhuma[nenhumaLente] == id_idade_presbiópico[presbiópico]:
       nenhuma_presbiópico.append(id_idade_presbiópico[presbiópico])
 
+rigida_astigmatismo = []
+rigida_prodLacReduzida = []
+rigida_diagHipermetrope = []
+rigida_jovem = []
+rigida_prePresbiopico = []
+rigida_presbiópico = []
 
-print('Nenhuma lente + astigmatismo: {}'.format(nenhuma_astigmatismo))
-print('Nenhuma lente + produção lacrimal reduzida: {}'.format(nenhuma_prodLacReduzida))
-print('Nenhuma lente + hipermetropia: {}'.format(nenhuma_diagHipermetrope))
-print('Nenhuma lente em jovens: {}'.format(nenhuma_jovem))
-print('Nenhuma lente em prePresbiopico: {}'.format(nenhuma_prePresbiopico))
-print('Nenhuma lente em presbiopico: {}'.format(nenhuma_presbiópico))
+for lenteRigida in range(len(id_rigida)):
+  for astig in range(len(id_astigmatismo)):
+    if id_rigida[lenteRigida] == id_astigmatismo[astig]:
+      rigida_astigmatismo.append(id_astigmatismo[astig])
+  for prodLacrimal in range(len(id_rigida)):
+    if id_rigida[lenteRigida] == id_producao_lacrimal[prodLacrimal]:
+      rigida_prodLacReduzida.append(id_producao_lacrimal[prodLacrimal])
+      # Output: Lente rígida + produção lacrimal reduzida: [7] ERR: output correto [7, 15]
+  for diagnost in range(len(id_diagnostico)):
+    if id_rigida[lenteRigida] == id_diagnostico[diagnost]:
+      rigida_diagHipermetrope.append(id_diagnostico[diagnost])
+  for jovem in range(len(id_idade_jovem)):
+    if id_rigida[lenteRigida] == id_idade_jovem[jovem]:
+      rigida_jovem.append(id_idade_jovem[jovem])
+  for prePresbiopico in range(len(id_idade_prePresbiopico)):
+    if id_rigida[lenteRigida] == id_idade_prePresbiopico[prePresbiopico]:
+      rigida_prePresbiopico.append(id_idade_prePresbiopico[prePresbiopico])
+  for presbiópico in range(len(id_idade_presbiópico)):
+    if id_rigida[lenteRigida] == id_idade_presbiópico[presbiópico]:
+      rigida_presbiópico.append(id_idade_presbiópico[presbiópico])
 
 print('='*60)
 print('Análise de variáveis por ID')
@@ -114,3 +135,33 @@ print('Astigmatismo:\n Total: {}\n IDs: {}\n'.format(len(id_astigmatismo), id_as
 print('Idade:\n Total:{}\n IDs: {}\n'.format(len(id_idades), id_idades))
 print('Diagnóstico:\n Total hipermetropes: {}\n Total míopes: {}\n IDs hipermetropes: {}\n'.format(len(id_diagnostico), 23-len(id_diagnostico), id_diagnostico))
 print('Produção lacrimal:\n Total reduzida: {}\n IDs reduzida: {}\n Total normal: {}\n'.format(len(id_producao_lacrimal), id_producao_lacrimal, 23-len(id_producao_lacrimal)))
+
+print('='*60)
+print('Lente: NENHUMA:')
+print('='*60)
+print('Nenhuma lente + astigmatismo: {}'.format(nenhuma_astigmatismo))
+print('Nenhuma lente + produção lacrimal reduzida: {}'.format(nenhuma_prodLacReduzida))
+print('Nenhuma lente + hipermetropia: {}'.format(nenhuma_diagHipermetrope))
+print('Nenhuma lente em jovens: {}'.format(nenhuma_jovem))
+print('Nenhuma lente em prePresbiopico: {}'.format(nenhuma_prePresbiopico))
+print('Nenhuma lente em presbiopico: {}'.format(nenhuma_presbiópico))
+
+print('='*60)
+print('Lente: RÍGIDA:')
+print('='*60)
+print('Lente rígida + astigmatismo: {}'.format(rigida_astigmatismo))
+print('Lente rígida + produção lacrimal reduzida: {}'.format(rigida_prodLacReduzida))
+print('Lente rígida + hipermetropia: {}'.format(rigida_diagHipermetrope))
+print('Lente rígida em jovens: {}'.format(rigida_jovem))
+print('Lente rígida em prePresbiopico: {}'.format(rigida_prePresbiopico))
+print('Lente rígida em presbiopico: {}'.format(rigida_presbiópico))
+
+#print('='*60)
+#print('Lente: NENHUMA:')
+#print('='*60)
+#print('Nenhuma lente + astigmatismo: {}'.format#(nenhuma_astigmatismo))
+#print('Nenhuma lente + produção lacrimal reduzida: {}#'.format(nenhuma_prodLacReduzida))
+#print('Nenhuma lente + hipermetropia: {}'.format#(nenhuma_diagHipermetrope))
+#print('Nenhuma lente em jovens: {}'.format#(nenhuma_jovem))
+#print('Nenhuma lente em prePresbiopico: {}'.format#(nenhuma_prePresbiopico))
+#print('Nenhuma lente em presbiopico: {}'.format#(nenhuma_presbiópico))
